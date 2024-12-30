e271e9b7f95285087bb8d52b9340cfe037f5819a
jd-morrow <jdianemorrow@gmail.com>
2024-12-29 20:50:18 -0500

    added 'updatable' and 'drawable' groups and added Player class to both

--- main.py
+++ main.py
@@ -6,18 +6,25 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        for object in updatable:
+            object.update(dt)
+
         screen.fill(000000)
-        player.draw(screen)
-        
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
